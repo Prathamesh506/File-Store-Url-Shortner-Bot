d0a6a8e1a6310de46ee0aeeb51825b4e8ccd4ed9
Shadow <145957007+misterrnova@users.noreply.github.com>
2024-02-06 18:42:23 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -1,11 +1,1 @@
 
-            
-# import asyncio
-
-# async def main():
-#     link = "https://www.youtube.com/watch?v=LCrkRTMkmzE"
-#     slink = await get_shortlink(link)
-#     print(slink)
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
